e1688c61f923fe6be5f8755668eef8c3f073269b
Travis Craft <tlcraft@users.noreply.github.com>
2021-05-18 13:13:04 -0600

    Create an Example class.

--- src/example.py
+++ src/example.py
@@ -1,2 +1,13 @@
-def add_one(x):
-    return x + 1
+from services.string_service import StringService 
+
+class Example:
+    def __init__(self, stringService: StringService):
+        self.stringService = stringService
+
+    def add_one(self, x: int):
+        return x + 1
+
+    def get_count(self):
+        number = self.add_one(1)
+        string = self.stringService.append("Count: ", number)
+        return string

55e33dac0dec34b225ca4d9d7720a861605bd834
Travis Craft <tlcraft@users.noreply.github.com>
2021-05-23 10:55:16 -0600

    Rename file, adjust import.

--- src/example_module/example_class.py
+++ src/example_module/example_class.py
@@ -1,7 +1,7 @@
-from services.string_service import StringService 
+from services import string_service
 
 class Example:
-    def __init__(self, stringService: StringService):
+    def __init__(self, stringService: string_service.StringService):
         self.stringService = stringService
 
     def add_one(self, x: int):

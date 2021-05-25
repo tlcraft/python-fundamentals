32ac8220461ce5dfa56ad4df839ffa70a3374b5d
Travis Craft <tlcraft@users.noreply.github.com>
2021-05-25 15:11:29 -0600

    Add type annotations for return values.

--- src/example_module/example_class.py
+++ src/example_module/example_class.py
@@ -4,10 +4,10 @@ class Example:
     def __init__(self, stringService: string_service.StringService):
         self.stringService = stringService
 
-    def add_one(self, x: int):
+    def add_one(self, x: int) -> int:
         return x + 1
 
-    def get_count(self):
+    def get_count(self) -> str:
         number = self.add_one(1)
         string = self.stringService.append("Count: ", number)
         return string
--- src/services/string_service.py
+++ src/services/string_service.py
@@ -1,3 +1,3 @@
 class StringService:
-    def append(self, prefix: str, postfix: str):
+    def append(self, prefix: str, postfix: str) -> str:
         return str(prefix) + str(postfix)

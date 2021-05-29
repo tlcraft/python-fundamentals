d6c1d376da3e2a1806c0b6d3bb1d022b5317ae62
Travis Craft <tlcraft@users.noreply.github.com>
2021-05-29 12:20:14 -0600

    Mock service in unit test.

--- src/example_module/test_example.py
+++ src/example_module/test_example.py
@@ -1,8 +1,14 @@
 from example_module.example_class import Example
-from services.string_service import StringService
+from unittest import TestCase, mock
 
-def test_should_add_one():
-    service = StringService()
-    example = Example(service)
-    assert example.add_one(3) == 4
+@mock.patch('example_module.example_class.StringService', autospec=True)
+class ExampleTestCase(TestCase):
+    def test_should_add_one(self, mock_service):
+        example = Example(mock_service)
+        assert example.add_one(3) == 4
 
+    def test_should_get_count(self, mock_service):
+        mock_service.append = mock.Mock(return_value="TestAppend")
+        example = Example(mock_service)
+        assert example.get_count() == "TestAppend"
+        mock_service.append.assert_called_once_with("Count: ", 2)
